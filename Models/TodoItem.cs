e94ff415088a6edb47b4b8b8b46b3749b45c3e53
Ioane <ioanelomidze7@gmail.com>
2020-10-18 21:38:06 +0400

    ef relations

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool isComplete { get; set; }
+        
+        [ForeignKey("TodoUserId")]
+        public long TodoUserId { get; set; }
+        public TodoUser User { get; set; }
+
+    }
+}
